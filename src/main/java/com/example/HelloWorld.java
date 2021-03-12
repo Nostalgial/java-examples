970864197390696d2786c895b2a037d03cf97153
86157 <17909077785@qq.com>
2021-03-13 01:44:25 +0800

    add

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+    public static int add(int x, int y) {
+        return x + y;
+    }
 }
